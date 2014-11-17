a50440d8895fc3710a29ba2111317f034d6004c0
r3pwn <wickett06@gmail.com>
2014-11-17 14:48:36 -0500

    Initial commit

---
+++ Wearable/src/main/java/com/r3pwn/wearflashlight/WearableActivity.java
@@ -0,0 +1,14 @@
+package com.r3pwn.wearflashlight;
+
+import android.app.*;
+import android.os.*;
+
+public class WearableActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wearable);
+    }
+}
